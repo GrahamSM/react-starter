3ea645f5767b9b18d96165eac361d69b0b6e9465
Rohan Nair <r@rohannair.ca>
2016-03-21 22:28:24 -0400

    Initial commit; react + webpack + hmre + postcss

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './app.css';
+
+const App = (props) => {
+  return (
+    <div className='start'>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App
